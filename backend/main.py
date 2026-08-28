c7d49cb772ca176552540d1d3779f57f7297a611
Mohammad Rehan Tabish <YOUR_GITHUB_EMAIL>
2026-08-28 19:49:58 +0530

    added path config

--- backend/main.py
+++ backend/main.py
@@ -17,8 +17,13 @@ BASE_DIR = os.path.dirname(
     os.path.abspath(__file__)
 )
 
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
+
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 
 
 # ============================================================
